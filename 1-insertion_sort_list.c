78ff84e2c9ac0b49ea058b11085ecfe5ead5d61f
azzaomer <azzaomer70@gmail.com>
2024-04-30 00:17:52 +0300

    Insertion Sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -22,15 +22,14 @@ void insertion_sort_list(listint_t **list)
 	}
 }
 /**
- *swap_node - swap a node for his previous one
+ *swap_node - swap nodes
  *@node: node
  *@list: node list
- *Return: return a pointer to a node which was enter it
+ *Return: return a pointer to a node which was entered
  */
 listint_t *swap_node(listint_t *node, listint_t **list)
 {
 	listint_t *back = node->prev, *current = node;
-	/*NULL, 19, 48, 9, 71, 13, NULL*/
 
 	back->next = current->next;
 	if (current->next)
