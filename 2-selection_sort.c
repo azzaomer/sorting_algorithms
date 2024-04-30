ea6e2ce2632d0266d3ff89440dcff4edc8956a78
azzaomer <azzaomer70@gmail.com>
2024-04-30 12:28:59 +0300

    Selection Sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,26 +8,31 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
-	size_t min_id, swap;
-
+	int min_id, swap, flag = 0;
+	
+	if (array == NULL)
+		return;
 	for(i = 0; i < size; i++)
 	{
 		/* Find the minimum element
 		 * in unsorted array
 		 */
 		min_id = i;
+		flag = 0;
 		for(j = i + 1; j < size; j++){
 		{
 			if(array[j] < array[min_id])
+			{
 				min_id = j;
+				flag += 1;
+			}
 		}
-		if(min_id != i)
-		{
-			swap = array[i];
-			array[i] = array[min_id];
-			array[min_id] = swap;
-		}
-		print_array(array, size);
+		
+		swap = array[i];
+		array[i] = array[min_id];
+		array[min_id] = swap;
+		if (flag != 0)
+			print_array(array, size);
 	}
 	}
 }
