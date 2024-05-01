46212819c0d2f6a91673829cfb4422890f27ed70
azzaomer <azzaomer70@gmail.com>
2024-05-01 15:43:10 +0300

    Bubble Sort

--- print_array.c
+++ print_array.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints array elements
+ * print_array - Prints an array of integers
  *
  * @array: The array to be printed
- * @size: Number of elements
+ * @size: Number of elements in @array
  */
 void print_array(const int *array, size_t size)
 {
     size_t i;
-    i = 0;
 
+    i = 0;
     while (array && i < size)
     {
         if (i > 0)
