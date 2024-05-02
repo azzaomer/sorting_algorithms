8bd1f3323653169ff1306b30b6d3563a632f5b69
azzaomer <azzaomer70@gmail.com>
2024-05-02 15:35:18 +0300

    Shell Sort

--- sort.h
+++ sort.h
@@ -25,4 +25,5 @@ void swap(int *array, size_t a, size_t b);
 void quick_sort(int *array, size_t size);
 void quick_alg(int *arr, int left, int right, size_t size);
 int partition(int *arr, int left, int right, size_t size);
+void shell_sort(int *array, size_t size);
 #endif
