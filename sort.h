cc71d57dccc03f500025170a7637831ecd414d56
azzaomer <azzaomer70@gmail.com>
2024-05-02 13:47:52 +0300

    Bubble Sort

--- sort.h
+++ sort.h
@@ -11,8 +11,8 @@
 typedef struct listint_s
 {
 	const int n;
-        struct listint_s *prev;
-        struct listint_s *next;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
